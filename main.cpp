ce02232f8ae05bb70860ec7c93e8e5be8ceff174
miron <miron.szewczyk@autonomik.pl>
2018-02-02 23:30:39 +0100

    Zaczyna dzialac

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include "state.cpp"
-#include "action.cpp"
 #include <string>
 using namespace std;
 int main() {
+
+
+
+    try {
+        connection C("postgres://hnmgputi:fA9vDEgP8zRS5VSGn1GIlTWIYylJB0HS@dumbo.db.elephantsql.com:5432/hnmgputi");
+
+        if (C.is_open()) {
+            cout << "Opened database successfully: " << C.dbname() << endl;
+        } else {
+            cout << "Can't open database" << endl;
+            return 1;
+        }
+        C.disconnect();
+    }
+        catch (const std::exception &e) {
+            cerr << e.what() << std::endl;
+            return 1;
+        }
+
+
     State state1;
-
-
     while(state1.shouldContinue()){
+        cout <<"Type command (type H for help)\n";
         string line;
         string type;
         getline(cin,line);
+        cout <<"["+line+"]\n";
         state1.doAction(line);
     }
     return 0;
--- state.cpp
+++ state.cpp
@@ -1,8 +1,10 @@
 #ifndef state
 #define state
 
-#include <pqxx/pqxx>
+#include <stdlib.h>
 #include <string>
+#include <cstring>
+#include <pqxx/pqxx>
 
 using namespace std;
 using namespace pqxx;
@@ -14,33 +16,85 @@ private:
     string password = "x";
     string hostadrr = "10.1.0.5";
     string port = "5432";
-    int lastCreatedOrder = "-1";
+    int lastCreatedOrder = -1;
+    connection C;
 
-    connection C("dbname = " + dbname + " user = " + user + " password = " + password + " hostaddr = " + hostadrr +" port = " + port);
-    work W(C);
+//    connection C("dbname = " + dbname + " user = " + user + " password = " + password + " hostaddr = " + hostadrr +" port = " + port);
+//    pqxx::connection C(port);
+//    work W(C);
 public:
+    State() : C("postgres://hnmgputi:fA9vDEgP8zRS5VSGn1GIlTWIYylJB0HS@dumbo.db.elephantsql.com:5432/hnmgputi") {};
+
     bool shouldContinue() {
         return true;
     }
 
-    Work giveWork() {
+    void doRealAction(string type, string args) {
+        if (type == "NO") {//NEW ORDER
+            int id_klienta;
+            sscanf(args.c_str(), "%d", &id_klienta);
+            {
+                work W(C);
+                W.exec(string("INSERT INTO ZAMOWIENIE (id_klienta,udane) VALUES (")
+                       + to_string(id_klienta) + string(",false);"));
+                W.commit();
+            }
+            {
+                nontransaction N(C);
+//            result r = W.exec("SELECT id_klienta from KLIENT WHERE udane = false;");
+                result r(N.exec("SELECT id_klienta from KLIENT WHERE udane = false;"));
+                lastCreatedOrder = (r.front())[0].as<int>();
+            }
+        } else if (type == "AO") { //ADD TO ORDER
+            work W(C);
+            int id_zamowienia, ilosc, id_pizzy;
+            sscanf(&args[0], "%d %d %d", &id_zamowienia, &id_pizzy, &ilosc);
+            W.exec(string("INSERT INTO PolaczeniaPizzaZamowienie (id_zamowienia,id_pizzy,ilosc)"
+                                  "VALUES (") + to_string(id_zamowienia) + string(",") + to_string(id_pizzy)
+                   + string(",") + to_string(ilosc) + string(");"));
+            result r = W.exec(string("SELECT sprawdzSkladniki(") + to_string(id_zamowienia) + string(");"));
+            //TODO sprawdz to
+            if (r.front()[0].as<bool>() == false) {
+                W.exec("DELETE from PolaczeniePizzaZamowienie WHERE id_zamowienia = " + to_string(id_zamowienia)
+                       + " AND id_pizzy = " + to_string(id_pizzy) + ";");
+            } else{
+                W.abort();
+            }
+        } else if (type == "FO") {//FINISH ORDER
+            int id_zamowienia;
+            work W(C);
+            sscanf(&args[0], "%d", &id_zamowienia);
+            W.exec("SELECT uczynZamowienie(" + to_string(id_zamowienia) + ")");
+            W.commit();
+        } else if (type == "D") {//DELIVER
+            work W(C);
+            W.exec("UPDATE Skladniki SET ilosc = ilosc+10");
+        } else if (type == "M") {//MENU
+            work W(C);
+            result r = W.exec("SELECT nazwa_pizzy,id_pizzy FROM Pizze;");
+            for (auto raw: r) {
+                cout << raw[1] << " " << raw[0] << "\n";
+            }
 
+        } else if (type == "KI") { //gives klient id, may add him
+
+
+        } else if (type == "END") {
+
+        } else if (type == "H") {
+
+        }
     }
 
-    void setLastOrder(int id) {
-        lastCreatedOrder = id;
-    }
+    void doAction(string line) {
+        char type[30], args[30];
 
-    void doAction(string &line) {
-        string type, args;
-
-        sscanf(line, "%s%[^\n]s", type, args);
-        cout << "type: " << type << " args: (" << args << ")\n";
-        Action action(*this, type, args,W);
-
+        sscanf(line.c_str(), "%s %s", type, args);
+        printf("((%s,%s))\n", type, args);
+        cout << "type: (" << type << ") args: (" << args << ")\n";
+        doRealAction(string(type), string(args));
     }
 
 };
 
-
-#endif state
+#endif// state
